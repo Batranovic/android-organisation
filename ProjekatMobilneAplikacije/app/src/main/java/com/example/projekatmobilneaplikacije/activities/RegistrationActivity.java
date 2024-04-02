54a8fb6058d07a277ce627d3344aa9ce23502ea1
Nina <nina.batranovic3@gmail.com>
2024-04-02 19:50:33 +0200

    fix

--- ProjekatMobilneAplikacije/app/src/main/java/com/example/projekatmobilneaplikacije/activities/RegistrationActivity.java
+++ ProjekatMobilneAplikacije/app/src/main/java/com/example/projekatmobilneaplikacije/activities/RegistrationActivity.java
@@ -16,55 +16,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.projekatmobilneaplikacije.R;
+import com.example.projekatmobilneaplikacije.databinding.ActivityRegistrationBinding;
 import com.example.projekatmobilneaplikacije.fragments.RegisterFragment;
+import com.example.projekatmobilneaplikacije.fragments.RegistrationFragment;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private ActivityRegistrationBinding binding;
     private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        ImageView addImageButton = findViewById(R.id.add_image_button);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_registration_container, new RegistrationFragment())
+                .commit();
 
-        addImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
 
-        //pronalazim dugme registePUP koje se nalazi u RegistrationActivity
-        findViewById(R.id.registerPUP).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment registerFragment = new RegisterFragment();
 
-                //R.id.registration je definisan u activity_registration.xml
-                //pritiskom na dugme registerPUP prelazim sa activity_registration na registerFragment
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.registration, registerFragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-    }
-
-    private void openGallery() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri selectedImageUri = data.getData();
-            ImageView imageView = findViewById(R.id.add_image_button);
-            imageView.setImageURI(selectedImageUri);
-        }
-    }
 }
