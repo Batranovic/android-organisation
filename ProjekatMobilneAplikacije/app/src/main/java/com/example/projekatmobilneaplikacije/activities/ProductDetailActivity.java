61ccc2406612544c2e6e836710cfda19e0d1f0dd
ayadjo <andjela1108@gmail.com>
2024-05-05 20:30:10 +0200

    feat: edit product

--- ProjekatMobilneAplikacije/app/src/main/java/com/example/projekatmobilneaplikacije/activities/ProductDetailActivity.java
+++ ProjekatMobilneAplikacije/app/src/main/java/com/example/projekatmobilneaplikacije/activities/ProductDetailActivity.java
@@ -149,5 +149,63 @@ public class ProductDetailActivity extends AppCompatActivity {
     }
 
 
-   
+    private void editProduct(String productId) {
+        String newTitle = titleEditText.getText().toString();
+        String newDescription = descriptionEditText.getText().toString();
+        String newSubcategory = subcategoryEditText.getText().toString();
+        String newEventType = eventTypeEditText.getText().toString();
+        int newPrice = Integer.parseInt(priceEditText.getText().toString());
+        String newAvailability = availabilityEditText.getText().toString();
+        String newVisibility = visibilityEditText.getText().toString();
+        // Pronalaženje dokumenta sa odgovarajućim productId
+        db.collection("products")
+                .whereEqualTo("id", productId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot querySnapshot = task.getResult();
+                            if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                DocumentSnapshot document = querySnapshot.getDocuments().get(0); // Uzmemo prvi dokument (ako ima više, uzmemo prvi)
+                                String documentId = document.getId();
+                                Log.d("","Document id: " + documentId);
+
+                                // Mapa koja sadrži nove vrijednosti polja
+                                Map<String, Object> updates = new HashMap<>();
+                                updates.put("title", newTitle);
+                                updates.put("description", newDescription);
+                                updates.put("subcategory", newSubcategory);
+                                updates.put("eventType", newEventType);
+                                updates.put("price", newPrice);
+                                updates.put("availability", newAvailability);
+                                updates.put("visibility", newVisibility);
+
+
+                                // Ažuriranje dokumenta s novim vrijednostima polja
+                                db.collection("products")
+                                        .document(documentId)
+                                        .update(updates)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(ProductDetailActivity.this, "Product updated", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(ProductDetailActivity.this, "Failed to update product", Toast.LENGTH_SHORT).show();
+                                                Log.d("ProductDetailActivity", "Error updating product", e);
+                                            }
+                                        });
+                            } else {
+                                Log.d("ProductDetailActivity", "No document found with productId: " + productId);
+                            }
+                        } else {
+                            Log.d("ProductDetailActivity", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
 }
